43f461d14bc6f2630e17490b9409662a18bc4d48
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-05-11 14:25:44 -0700

    Created the Connection for shared DB

--- test/test.py
+++ test/test.py
@@ -4,21 +4,18 @@ import os
 
 
 
-# pw = getpass.getpass(prompt = 'Input the Password:')  
-pw = "Liam/Ryan9904"
+pw = getpass.getpass(prompt = 'Input the Password:')  
 
-##connection_directory = os.path.join(current_directory, '..', 'Connection')
-connection_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Connection'))
-##config_file_path = os.path.join(current_directory, 'Connection')
+connection_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 print(connection_dir)
 
 try:
     connection=oracledb.connect(
-     config_dir=r"/conn",
+     config_dir=connection_dir + r"/conn",
      user="admin",
      password= pw,
      dsn="wizarddb_tpurgent",
-     wallet_location=r"/conn",
+     wallet_location=connection_dir + r"/conn",
      wallet_password=pw)
 
 except Exception as err:
