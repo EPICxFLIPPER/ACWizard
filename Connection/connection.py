81a91a47a0a2ab558894ae997bacd4a54183a451
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-05-12 10:29:24 -0700

    Created a folder for queries, and allowed the connection to be imported

--- Connection/connection.py
+++ Connection/connection.py
@@ -3,25 +3,29 @@ import oracledb
 import os
 
 
+    
+    ##Effects: Returns the connection to the Oracle Database
+def getConnection():
 
-pw = getpass.getpass(prompt = 'Input the Password:')  
+    pw = getpass.getpass(prompt = 'Input the Password:')  
 
-connection_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-print(connection_dir)
+    connection_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    print(connection_dir)
 
-try:
-    connection=oracledb.connect(
-     config_dir=connection_dir + r"/conn",
-     user="admin",
-     password= pw,
-     dsn="wizarddb_tpurgent",
-     wallet_location=connection_dir + r"/conn",
-     wallet_password=pw)
+    try:
+        connection=oracledb.connect(
+        config_dir=connection_dir + r"/conn",
+        user="admin",
+        password= pw,
+        dsn="wizarddb_tpurgent",
+        wallet_location=connection_dir + r"/conn",
+        wallet_password=pw)
 
-except Exception as err:
-    print("Error connecting to DB", err)
-else:
-    print(connection.version)
+    except Exception as err:
+        print("Error connecting to DB", err)
+    else:
+        print(connection.version)
+        return connection
 
 
 
