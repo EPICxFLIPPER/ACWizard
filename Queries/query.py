cc52f71f7cc700e4d9be4f113dbd8eff793db551
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-05-12 16:13:51 -0700

    Added single house selection method

--- Queries/query.py
+++ Queries/query.py
@@ -1,36 +1,50 @@
 import sys
 import os
 
-# Add the parent directory to the Python module search path
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
-# Now import the Connection module
 from Connection.connection import getConnection
 
-# Now you can use the Connection class
-connection = getConnection()
 
-cursor = connection.cursor()
 
-try:
-    # Execute the insert statement
 
-    # Fetch all rows from the Houses table
-    cursor.execute('SELECT * FROM Houses')
-    results = cursor.fetchall()
+##Effects: Selects all from Houses table
+def selectAll():
+    try:
+        connection = getConnection()
+        cursor = connection.cursor()
+        
+        cursor.execute('SELECT * FROM Houses')
+        results = cursor.fetchall()
 
-    # Print the number of rows fetched
-    print("Number of rows fetched:", len(results))
+        print("Number of rows fetched:", len(results))
 
-    # Print the fetched rows
-    for row in results:
-        print(row)
+        # Print the fetched rows
+        for row in results:
+            print(row)
 
-except Exception as e:
-    print("Error:", e)
+    except Exception as e:
+        print("Error:", e)
 
-finally:
-    # Close the cursor and connection
-    cursor.close()
-    connection.close()
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+##Effects, prints the house at specified block and lot number
+def selectSingle(block, lot):
+    try:
+        connection = getConnection()
+        cursor = connection.cursor()
+        query = 'SELECT * FROM Houses WHERE Lot = :lot AND Block = :block'
+        cursor.execute(query, {'lot': lot, 'block': block})
+        results = cursor.fetchall()
+        print(results)
+        print(len(results))
+        return results
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        cursor.close()
+        connection.close()
