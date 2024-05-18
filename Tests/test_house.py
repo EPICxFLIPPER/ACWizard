8b29ac9834e8701ec075fcc2278119b9331f4070
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-05-17 21:01:14 -0600

    created testing stubs

--- Tests/test_house.py
+++ Tests/test_house.py
@@ -4,3 +4,31 @@ import pytest
 
 def test_simple():
     assert 1 + 1 == 2
+
+@pytest.mark.skip(reason="This test is not done")
+def test_Elevation3Across():
+    print("stub")
+
+@pytest.mark.skip(reason="This test is not done")
+def test_Elevation7Across():
+    print("stub")
+
+@pytest.mark.skip(reason="This test is not done")
+def test_Elevation30PercentRule():
+    print("stub")
+
+@pytest.mark.skip(reason="This test is not done")
+def test_Elevation3InRow():
+    print("stub")
+
+@pytest.mark.skip(reason="This test is not done")
+def test_Elevation2Appart():
+    print("stub")
+
+@pytest.mark.skip(reason="This test is not done")
+def test_ElevationConers():
+    print("stub")
+
+@pytest.mark.skip(reason="This test is not done")
+def test_elevationGeneral():
+    print("stub")
