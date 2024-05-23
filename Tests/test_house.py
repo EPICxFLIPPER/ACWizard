e32172f1d2d50536b119dd9d41ffe9a06f572200
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-05-23 12:24:15 -0600

    Updated block numbers in tests, made tests have two in left, two in right

--- Tests/test_house.py
+++ Tests/test_house.py
@@ -22,39 +22,39 @@ def test_Elevation3AcrossMix():
 
 #All 3 houses across have CL elevation
 def test_Elevation3AcrossSame():
-    cross = [(n,1,5),(n,1,6),(n,1,7)]
-    opp = House(n,1,8,cross,[],[],[],[])
+    cross = [(n,2,5),(n,2,6),(n,2,7)]
+    opp = House(n,2,8,cross,[],[],[],[])
     assert sorted(opp.elevations()) == sorted(["CR","PR"])
 
 
 ##All 3 houses across have different elevation, expect empty return
 def test_Elevation3AcrossAll():
-    cross = [(n,1,9),(n,1,10),(n,1,11)]
-    opp = House(n,1,12,cross,[],[],[],[])
+    cross = [(n,3,9),(n,3,10),(n,3,11)]
+    opp = House(n,3,12,cross,[],[],[],[])
     assert opp.elevations() == []
 
 ##All 3 houses across have no set elevation
 def test_Elevation3AcrossEmpty():
-    cross = [(n,1,13),(n,1,14),(n,1,15)]
+    cross = [(n,4,13),(n,4,14),(n,4,15)]
     opp = House(n,1,16,cross,[],[],[],[])
     assert sorted(opp.elevations()) == sorted(["CR","PR","CL"])
 
 ##Only two of the across houses have set elevations
 def test_Elevation3Across2Set():
-    cross = [(n,1,17),(n,1,18),(n,1,19)]
-    opp = House(n,1,20,cross,[],[],[],[])
+    cross = [(n,5,17),(n,5,18),(n,5,19)]
+    opp = House(n,5,20,cross,[],[],[],[])
     assert opp.elevations() == ["PR"]
 
 ##4 Houses Across "CL" and "CR" Elevations
 def test_Elevation4Across():
-    cross = [(n,1,21),(n,1,22),(n,1,23),(n,1,24)]
-    opp = House(n,1,25,cross,[],[],[],[])
+    cross = [(n,6,21),(n,6,22),(n,6,23),(n,6,24)]
+    opp = House(n,6,25,cross,[],[],[],[])
     assert opp.elevations() == ["PR"]
 
 ##5 Houses Across all elevations used, empty expected
 def test_Elevation4Across():
-    cross = [(n,1,26),(n,1,27),(n,1,28),(n,1,29),(n,1,30)]
-    opp = House(n,1,31,cross,[],[],[],[])
+    cross = [(n,7,26),(n,7,27),(n,7,28),(n,7,29),(n,7,30)]
+    opp = House(n,7,31,cross,[],[],[],[])
     assert opp.elevations() == []
 
 ##Tests the case where a model elevation is under 30% of the block, importatnt that CL is in the list for this test
@@ -74,19 +74,19 @@ def test_Elevation30PercentRuleOver():
 
 ##Tests the case for 3 in row, with this house being the middle one
 def test_Elevation3InRow1EachSize():
-    left = [None,None,(n,100,2)]
-    right = [(n,100,4),None,None]
+    left = [None,(n,100,2)]
+    right = [(n,100,4),None]
     opp = House(n,100,1,[],left,right,[],[])
     assert opp.elevations() == ["CR"]
 ##Tests the case for 3 in row, with this house being the leftmost one
 def test_Elevation3InRow2Right():
-    right = [(n,100,2),(n,100,3),None]
+    right = [(n,100,2),(n,100,3)]
     opp = House(n,100,1,[],[],right,[],[])
     assert opp.elevations() == ["PR"]
 
 ##Tests the case for 3 in row, with this house being the rightmost one
 def test_Elevation3InRow2Left():
-    left = [None,(n,100,2),(n,100,3)]
+    left = [(n,100,2),(n,100,3)]
     opp = House(n,100,1,[],left,[],[],[])
     assert opp.elevations() == ["PR"]
 
