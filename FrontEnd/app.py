fdf25e1020276c092de8c85b5fbeed38b9c38301
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-06-08 10:36:23 -0600

    got the template html working with flask

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -2,24 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/FrontEnd')
+@app.route('/')
 def home():
     return render_template('index.html', message="Hello, Flask!")
-
-@app.route('/about')
-def about():
-    return "This is the about page."
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return f'User: {username}'
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return 'Handling POST request'
-    else:
-        return 'Handling GET request'
 
 if __name__ == '__main__':
     app.run(debug=True)
